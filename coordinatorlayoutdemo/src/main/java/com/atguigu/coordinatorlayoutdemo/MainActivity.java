f0bbc849d1c6376685ff08bcda9a38785365aa29
ljt474297694 <474297694@qq.com>
2017-03-23 11:11:40 +0800

    滑动 隐藏显示按钮

--- coordinatorlayoutdemo/src/main/java/com/atguigu/coordinatorlayoutdemo/MainActivity.java
+++ coordinatorlayoutdemo/src/main/java/com/atguigu/coordinatorlayoutdemo/MainActivity.java
@@ -7,8 +7,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.animation.Animation;
-import android.view.animation.ScaleAnimation;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -29,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
@@ -36,19 +35,12 @@ public class MainActivity extends AppCompatActivity {
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
                 if(Math.abs(verticalOffset)>(collapsingToolbarLayout.getHeight() - toolbar.getHeight())*0.75) {
                     if(isOpen) {
-                        ScaleAnimation animation =new ScaleAnimation(1,0,1,0,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-                        animation.setDuration(200);
-                        animation.setFillAfter(true);
-                        floatingActionButton.startAnimation(animation);
+                        floatingActionButton.hide();
                         isOpen = false;
                     }
-
                 }else{
                     if(!isOpen) {
-                        ScaleAnimation animation =new ScaleAnimation(0,1,0,1, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-                        animation.setDuration(200);
-                        animation.setFillAfter(true);
-                        floatingActionButton.startAnimation(animation);
+                        floatingActionButton.show();
                         isOpen = true;
                     }
                 }
